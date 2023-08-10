8292b60eccd808ec6adf81356cb38ea79b30d897
Clouds <clouds@millennia-solusi.id>
2023-08-10 13:24:36 +0000

    Update file Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -20,7 +20,7 @@
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
     git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
-    git checkout $$branch
+    git checkout $branch
     git reset --hard {{ $commit }}
 @endtask
 

5148331699414eea15218a5b941d3e0ce59b21e7
Clouds <clouds@millennia-solusi.id>
2023-08-10 14:42:01 +0000

    fix envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,7 +6,6 @@
     $app_dir = '/home/msi/app';
     $release = date('YmdHis');
     $new_release_dir = $releases_dir .'/'. $release;
-    $branch = 'dev'
 @endsetup
 
 @story('deploy')
@@ -18,7 +17,7 @@
 @task('clone_repository')
     echo 'Cloning repository'
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-    git clone {{ $repository }} {{ $new_release_dir }}
+    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
     git reset --hard {{ $commit }}
 @endtask

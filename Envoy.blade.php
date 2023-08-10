16d5145db08d114e5e68203823e878c3e3ef0b93
Clouds <clouds@millennia-solusi.id>
2023-08-10 18:18:35 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -17,7 +17,7 @@
 @task('clone_repository')
     echo 'Cloning repository'
     [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+    git clone {{ $repository }} {{ $new_release_dir }}
     cd {{ $new_release_dir }}
     git checkout dev
     git reset --hard {{ $commit }}

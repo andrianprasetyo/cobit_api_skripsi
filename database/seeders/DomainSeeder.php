46ae6297dd7ba0abf2e24e27a814c0133f3da94e
chandra <chandra@millennia-solusi.id>
2023-08-15 15:39:59 +0700

    update seeder domain

--- database/seeders/DomainSeeder.php
+++ database/seeders/DomainSeeder.php
@@ -13,7 +13,7 @@ class DomainSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
+        /*$data = [
             [
                 'kode' => 'EDM01',
             ],
@@ -29,6 +29,98 @@ class DomainSeeder extends Seeder
             [
                 'kode' => 'EDM05',
             ],
+        ];*/
+        $data=[
+            [
+                'kode'=>'APO06'
+            ],
+            [
+                'kode'=>'APO07'
+            ],
+            [
+                'kode'=>'APO08'
+            ],
+            [
+                'kode'=>'APO09'
+            ],
+            [
+                'kode'=>'APO10'
+            ],
+            [
+                'kode'=>'APO11'
+            ],
+            [
+                'kode'=>'APO12'
+            ],
+            [
+                'kode'=>'APO13'
+            ],
+            [
+                'kode'=>'APO14'
+            ],
+            [
+                'kode'=>'BAI01'
+            ],
+            [
+                'kode'=>'BAI02'
+            ],
+            [
+                'kode'=>'BAI03'
+            ],
+            [
+                'kode'=>'BAI04'
+            ],
+            [
+                'kode'=>'BAI05'
+            ],
+            [
+                'kode'=>'BAI06'
+            ],
+            [
+                'kode'=>'BAI07'
+            ],
+            [
+                'kode'=>'BAI08'
+            ],
+            [
+                'kode'=>'BAI09'
+            ],
+            [
+                'kode'=>'BAI10'
+            ],
+            [
+                'kode'=>'BAI11'
+            ],
+            [
+                'kode'=>'DSS01'
+            ],
+            [
+                'kode'=>'DSS02'
+            ],
+            [
+                'kode'=>'DSS03'
+            ],
+            [
+                'kode'=>'DSS04'
+            ],
+            [
+                'kode'=>'DSS05'
+            ],
+            [
+                'kode'=>'DSS06'
+            ],
+            [
+                'kode'=>'MEA01'
+            ],
+            [
+                'kode'=>'MEA02'
+            ],
+            [
+                'kode'=>'MEA03'
+            ],
+            [
+                'kode'=>'MEA04'
+            ]
         ];
 
         foreach ($data as $item) {

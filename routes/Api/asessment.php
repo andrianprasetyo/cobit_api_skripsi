ac655ad632c082a864fe5bcd85f4a5b92b9ee402
Agus <agus@millennia-solusi.id>
2023-08-23 04:56:57 +0000

    assesment

--- routes/Api/asessment.php
+++ routes/Api/asessment.php
@@ -3,6 +3,7 @@ Route::group(['middleware' => ['jwt.auth']], function ($router) {
     Route::get('/assesment/list', 'AsessmentController@list');
     Route::get('/assesment/detail/{id}', 'AsessmentController@detailByID');
     Route::post('/assesment/add', 'AsessmentController@add');
+    Route::put('/assesment/edit/{id}', 'AsessmentController@edit');
     Route::post('/assesment/responden/invite', 'AsessmentController@inviteRespondent');
     Route::post('/assesment/responden/invite-by-excel', 'AsessmentController@inviteRespondentByExcel');
 });

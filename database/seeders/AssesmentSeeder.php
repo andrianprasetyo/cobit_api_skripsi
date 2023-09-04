17b16fb4d5720b2621c86dc20b8a979edc180446
Agus <agus@millennia-solusi.id>
2023-09-04 02:06:13 +0000

    canvas

--- database/seeders/AssesmentSeeder.php
+++ database/seeders/AssesmentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Assesment;
 use App\Models\Organisasi;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -32,8 +33,10 @@ class AssesmentSeeder extends Seeder
 
         $assesment=new Assesment();
         $assesment->nama="Assesment Cobit 2023";
-        $assesment->tahun='2023';
+        // $assesment->tahun='2023';
         $assesment->organisasi_id=$organisasi->id;
+        $assesment->start_date=Carbon::now()->format('Y-m-d');
+        $assesment->end_date=Carbon::now()->addMonth()->format('Y-m-d');
         $assesment->save();
     }
 }

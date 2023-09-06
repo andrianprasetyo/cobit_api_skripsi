90313df26ac911af30ecf1b6886ea0b21e243074
Agus <agus@millennia-solusi.id>
2023-09-06 11:05:07 +0000

    capability

--- app/Http/Controllers/Api/CapabilityAssesmentController.php
+++ app/Http/Controllers/Api/CapabilityAssesmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CapabilityAssesment\CapabilityAssesmentResource;
 use App\Models\CapabilityAnswer;
+use App\Models\CapabilityAssesment;
 use App\Models\CapabilityLevel;
 use App\Traits\JsonResponse;
 use Illuminate\Http\Request;
@@ -27,6 +28,20 @@ class CapabilityAssesmentController extends Controller
 
     public function createAnswer(Request $request)
     {
-        return $this->successResponse($request->all());
+        $jawaban=$request->jawaban;
+        foreach ($jawaban as $_item_payload) {
+            $capabilityass=$_item_payload['capabilityass'];
+
+            $ass = new CapabilityAssesment();
+            if($capabilityass['id'] != null){
+                $ass=CapabilityAssesment::find($capabilityass['id']);
+            }
+            $ass->capability_level_id = $capabilityass['capability_level_id'];
+            $ass->capability_answer_id = $capabilityass['capability_answer_id'];
+            $ass->note = $capabilityass['note'];
+            $ass->ofi = $capabilityass['ofi'];
+            $ass->save();
+        }
+        return $this->successResponse();
     }
 }

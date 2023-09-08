256ac562412690631ef7c3db417aec672e3c49b4
Agus <agus@millennia-solusi.id>
2023-09-08 02:26:17 +0000

    capability

--- routes/Api/media-repository.php
+++ routes/Api/media-repository.php
@@ -3,4 +3,5 @@ Route::group(['middleware' => ['jwt.auth']], function ($router) {
     Route::get('/repository/list', 'MediaRepositoryController@list');
     Route::get('/repository/detail/{id}', 'MediaRepositoryController@detailByID');
     Route::post('/repository/add', 'MediaRepositoryController@add');
+    Route::delete('/repository/remove/{id}', 'MediaRepositoryController@remove');
 });

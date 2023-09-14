4e37a553356eb13e1a4938d167093514dfe0fb2d
Agus <agus@millennia-solusi.id>
2023-09-14 01:54:20 +0000

    Update AsessmentController.php

---
+++ app/Exports/AnalisaGapExport.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Contracts\View\View;
+
+class AnalisaGapExport implements FromView
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    private $data, $header;
+    public function __construct($data, $header)
+    {
+        $list = [];
+        $this->data = $data;
+        $this->header = $header;
+    }
+    public function view(): View
+    {
+        return view('report.quesionerresult', [
+            'header' => $this->header,
+            'hasil' => $this->data
+        ]);
+    }
+}

69a19c52fc5a3a160cf72de4ebe65ed6204690b6
Agus <agus@millennia-solusi.id>
2023-09-15 01:36:39 +0000

    domain

--- app/Http/Controllers/Api/CapabilityAssesmentController.php
+++ app/Http/Controllers/Api/CapabilityAssesmentController.php
@@ -167,12 +167,10 @@ class CapabilityAssesmentController extends Controller
         $evidents = $request->evident;
         $ofi = $request->ofi;
 
-        $_deb=[];
+        // $_deb=[];
         DB::beginTransaction();
         try {
 
-            // $tes_evi1 = [];
-            // $tes_evi2=[];
             for ($i = 0; $i < count($capability_assesment); $i++) {
                 // $capabilityass = $_item_payload['capabilityass'];
 
@@ -190,18 +188,14 @@ class CapabilityAssesmentController extends Controller
                 $capability_ass->ofi = $ofi[$i];
                 $capability_ass->assesment_id=$request->assesment_id;
                 $capability_ass->domain_id = $request->domain_id;
+                $capability_ass->save();
 
-                // $capability_ass->save();
-
-                // $tes_evi2[]= isset($evidents[$i])?$evidents[$i]:[];
                 if(isset($evidents[$i]) && count($evidents[$i]) > 0)
                 {
                     $evident = $evidents[$i];
-                    // $tes_evi1[] = $evident;
                     if (count($evident) > 0) {
-                        // CapabilityAssesmentEvident::where('capability_assesment_id', $capability_ass->id)->delete();
+                        CapabilityAssesmentEvident::where('capability_assesment_id', $capability_ass->id)->delete();
                         for ($r = 0; $r < count($evident); $r++) {
-                            // $evident_doc=
                             $_evident[] = array(
                                 'id'=>Str::uuid(),
                                 'capability_assesment_id' => $capability_ass->id,
@@ -210,14 +204,14 @@ class CapabilityAssesmentController extends Controller
                                 'media_repositories_id' => isset($evident[$r]['media_repositories_id']) ? $evident[$r]['media_repositories_id'] : null,
                             );
                         }
-                        // CapabilityAssesmentEvident::insert($_evident);
+                        CapabilityAssesmentEvident::insert($_evident);
                     }
                 }
 
                 if(isset($ofi[$i]) && count($ofi[$i]) > 0)
                 {
                     if (count($ofi) > 0) {
-                        // CapabilityAssesmentOfi::where('capability_assesment_id', $capability_ass->id)->delete();
+                        CapabilityAssesmentOfi::where('capability_assesment_id', $capability_ass->id)->delete();
                         $_ofi = [];
                         $ofi_item=$ofi[$i];
                         for ($o = 0; $o < count($ofi); $o++) {
@@ -229,8 +223,8 @@ class CapabilityAssesmentController extends Controller
                                 'domain_id' => isset($ofi_item[$o]['domain_id']) ? $ofi_item[$o]['domain_id'] : null,
                             );
                         }
-                        // CapabilityAssesmentOfi::insert($_ofi);
-                        $_deb[]=$_ofi;
+                        CapabilityAssesmentOfi::insert($_ofi);
+                        // $_deb[]=$_ofi;
                     }
                 }
             }
@@ -243,7 +237,7 @@ class CapabilityAssesmentController extends Controller
             // ]);
 
             DB::commit();
-            return $this->successResponse($_deb);
+            return $this->successResponse();
         } catch (\Exception $e) {
             DB::rollBack();
             return $this->errorResponse($e->getMessage());
